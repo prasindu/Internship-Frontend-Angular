a8b17b75a7344f75d33ab6c5ab3473286bbaaedb
prasindu <prasindudeshan1@gmail.com>
2026-09-21 02:28:45 +0530

    change ui

--- src/app/components/customer-dashboard/customer-dashboard.ts
+++ src/app/components/customer-dashboard/customer-dashboard.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, inject } from '@angular/core';
+import { Component, OnInit, inject, ChangeDetectorRef } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CustomerService, Customer } from '../../services/customer';
@@ -15,16 +15,18 @@ export class CustomerDashboardComponent implements OnInit {
   private customerService = inject(CustomerService);
   private fb = inject(FormBuilder);
   private router = inject(Router);
+  
+  
+  private cdr = inject(ChangeDetectorRef); 
 
   customers: Customer[] = [];
+  filteredCustomers: Customer[] = [];
   userName: string | null = '';
-  
   
   showModal = false;
   isEditing = false;
   editingId: number | null = null;
 
- 
   customerForm: FormGroup = this.fb.group({
     name: ['', Validators.required],
     email: ['', [Validators.required, Validators.email]],
@@ -40,13 +42,31 @@ export class CustomerDashboardComponent implements OnInit {
 
   loadCustomers() {
     this.customerService.getCustomers().subscribe({
-      next: (data) => this.customers = data,
+      next: (data) => {
+        this.customers = data;
+        this.filteredCustomers = data;
+        this.cdr.detectChanges(); 
+      },
       error: (err) => {
         if (err.status === 401) {
           this.logout();
         }
       }
     });
+  }
+
+  onSearch(event: any) {
+    const term = event.target.value.toLowerCase();
+    if (!term) {
+      this.filteredCustomers = this.customers;
+    } else {
+      this.filteredCustomers = this.customers.filter(c => 
+        c.id?.toString().includes(term) || 
+        c.name.toLowerCase().includes(term) || 
+        c.email.toLowerCase().includes(term)
+      );
+    }
+    this.cdr.detectChanges(); 
   }
 
   openModal(customer?: Customer) {
@@ -71,24 +91,43 @@ export class CustomerDashboardComponent implements OnInit {
     if (this.customerForm.invalid) return;
 
     if (this.isEditing && this.editingId) {
-      
-      this.customerService.updateCustomer(this.editingId, this.customerForm.value).subscribe(() => {
-        this.loadCustomers();
-        this.closeModal();
+      // Update
+      this.customerService.updateCustomer(this.editingId, this.customerForm.value).subscribe({
+        next: () => {
+          this.loadCustomers();
+          this.closeModal();
+        },
+        error: () => {
+    
+          this.loadCustomers();
+          this.closeModal();
+        }
       });
     } else {
-     
-      this.customerService.createCustomer(this.customerForm.value).subscribe(() => {
-        this.loadCustomers();
-        this.closeModal();
+      // Create
+      this.customerService.createCustomer(this.customerForm.value).subscribe({
+        next: () => {
+          this.loadCustomers();
+          this.closeModal();
+        },
+        error: () => {
+          this.loadCustomers();
+          this.closeModal();
+        }
       });
     }
   }
 
   deleteCustomer(id: number | undefined) {
     if (id && confirm('Are you sure you want to delete this customer?')) {
-      this.customerService.deleteCustomer(id).subscribe(() => {
-        this.loadCustomers();
+      this.customerService.deleteCustomer(id).subscribe({
+        next: () => {
+          this.loadCustomers();
+        },
+        error: () => {
+       
+          this.loadCustomers();
+        }
       });
     }
   }
