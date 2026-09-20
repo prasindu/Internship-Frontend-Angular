81c4aff7cab19a440043bfd34c620b1e4af377ea
prasindu <prasindudeshan1@gmail.com>
2026-09-21 01:29:03 +0530

    feat: setup login UI and auth service integration

---
+++ src/app/components/login/login.ts
@@ -0,0 +1,38 @@
+import { Component, inject } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Router, RouterModule } from '@angular/router';
+import { AuthService } from '../../services/auth'; 
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, ReactiveFormsModule, RouterModule],
+  templateUrl: './login.html',
+  styleUrls: ['./login.css']
+})
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+  private authService = inject(AuthService);
+  private router = inject(Router);
+
+  loginForm: FormGroup = this.fb.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required]
+  });
+  
+  errorMessage = '';
+
+  onSubmit() {
+    if (this.loginForm.valid) {
+      this.authService.login(this.loginForm.value).subscribe({
+        next: () => {
+          this.router.navigate(['/dashboard']);
+        },
+        error: (err) => {
+          this.errorMessage = 'Invalid email or password';
+        }
+      });
+    }
+  }
+}
---
+++ src/app/services/auth.ts
@@ -0,0 +1,25 @@
+import { inject, Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable, tap } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  private http = inject(HttpClient);
+  
+  
+  private apiUrl = 'https://localhost:7196/api/Auth'; 
+
+  login(data: any): Observable<any> {
+    return this.http.post(`${this.apiUrl}/login`, data).pipe(
+      tap((response: any) => {
+        if (response && response.token) {
+          
+          localStorage.setItem('token', response.token);
+          localStorage.setItem('userName', response.name);
+        }
+      })
+    );
+  }
+}
